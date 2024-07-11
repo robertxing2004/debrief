a14077e54ba2397e7bdac8a05d88604f1c1b66bb
RobertXing <rxing24@uwo.ca>
2024-07-10 22:21:28 -0400

    refactor(user-page): profile info is now a component

---
+++ src/app/[username]/profile.tsx
@@ -0,0 +1,53 @@
+import { redirect } from "next/navigation";
+import { getUser, lucia } from "../../lib/auth";
+import { cookies } from "next/headers";
+
+export default async function Profile({ params }: { params: { username: string } }) {
+  
+  async function logout(): Promise<ActionResult> {
+    "use server";
+    const session = await getUser();
+    if (!session) {
+      return {
+        error: "Unauthorized"
+      };
+    }
+  
+    await lucia.invalidateSession(session.id);
+  
+    const sessionCookie = lucia.createBlankSessionCookie();
+    cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+    return redirect("/");
+  }
+
+  // check lucia for user session
+  const user = await getUser();
+	if (!user) {
+		redirect("/login");
+	}
+
+  else {
+    const res = await fetch(`http://localhost:3000/api/user/${params.username}`);
+    const data = await res.json();
+
+    return (
+      <main>
+        <div>
+          <h1 className="">this is the user page for {params.username}</h1>
+        </div>
+        <div>
+          <img className="rounded-full w-12" src={data.avatar_url}/>
+        </div>
+        <div>
+          <form action={logout}>
+            <button>Sign out</button>
+          </form>
+        </div>
+      </main>
+    );
+  }
+}
+
+interface ActionResult {
+	error: string | null;
+}
